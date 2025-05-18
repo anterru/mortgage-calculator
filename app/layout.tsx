82046dc508b6a0ca5e929b9f6747781acfa7e613
Toni <antonio.rubio@audibene.de>
2025-05-18 23:58:22 +0200

    Change title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Mortgage Calculator',
+  description: 'Calculator for your mortgage, rental income, and taxes in Spain',
+  generator: 'anterru',
 }
 
 export default function RootLayout({

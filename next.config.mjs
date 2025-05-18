b64579182fc620e878e3c9b70b3d88b6d67bc6d0
Toni <antonio.rubio@audibene.de>
2025-05-18 22:53:18 +0200

    Publish to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: '/mortgage-calculator',
+  basePath: '/mortgage-calculator',
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,6 +11,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
 }
 
 export default nextConfig
